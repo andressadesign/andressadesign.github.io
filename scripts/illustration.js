c9b1de8c4ff3856a688f7bd4660d86d8ef08e7ce
andressadesign <86840379+andressadesign@users.noreply.github.com>
2021-07-14 17:12:22 -0300

    Mudança ordem

--- scripts/illustration.js
+++ scripts/illustration.js
@@ -1,15 +1,16 @@
 let thumbnailsJSON = [
     {tn:'<img onclick="openIMG(this)" src="../assets/gallery/thumb/cartola.png" data-img="../assets/gallery/img/cartola.png">'},
     {tn:'<img onclick="openIMG(this)" src="../assets/gallery/thumb/faroeste.png" data-img="../assets/gallery/img/faroeste.png">'},
-    {tn:'<img onclick="openIMG(this)" src="../assets/gallery/thumb/arara.png" data-img="../assets/gallery/img/arara.png">'},
     {tn:'<img onclick="openIMG(this)" src="../assets/gallery/thumb/ayeron.png" data-img="../assets/gallery/img/ayeron.png">'},
-    {tn:'<img onclick="openIMG(this)" src="../assets/gallery/thumb/christ.png" data-img="../assets/gallery/img/christ.png">'},
-    {tn:'<img onclick="openIMG(this)" src="../assets/gallery/thumb/coragem.png" data-img="../assets/gallery/img/coragem.png">'},
     {tn:'<img onclick="openIMG(this)" src="../assets/gallery/thumb/coringa.png" data-img="../assets/gallery/img/coringa.png">'},
+    {tn:'<img onclick="openIMG(this)" src="../assets/gallery/thumb/arara.png" data-img="../assets/gallery/img/arara.png">'},
     {tn:'<img onclick="openIMG(this)" src="../assets/gallery/thumb/desconhecido.png" data-img="../assets/gallery/img/desconhecido.png">'},
+    {tn:'<img onclick="openIMG(this)" src="../assets/gallery/thumb/coragem.png" data-img="../assets/gallery/img/coragem.png">'},
     {tn:'<img onclick="openIMG(this)" src="../assets/gallery/thumb/easy.png" data-img="../assets/gallery/img/easy.png">'},
-    {tn:'<img onclick="openIMG(this)" src="../assets/gallery/thumb/velho.png" data-img="../assets/gallery/img/velho.png">'},
+    {tn:'<img onclick="openIMG(this)" src="../assets/gallery/thumb/christ.png" data-img="../assets/gallery/img/christ.png">'},
     {tn:'<img onclick="openIMG(this)" src="../assets/gallery/thumb/negra.png" data-img="../assets/gallery/img/negra.png">'},
+    {tn:'<img onclick="openIMG(this)" src="../assets/gallery/thumb/velho.png" data-img="../assets/gallery/img/velho.png">'},
+   
     
 
     ];
